8429dd8104e17c24bcd3e78e8d1d1bb7a5f99549
Peter Hintze <hintze.peter@gmail.com>
2025-02-01 10:45:29 -0500

    Browser Routing established

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,15 @@
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import Shop from "./pages/Shop"
 import reactLogo from "@/assets/react.svg"
-import { Button, Center, HStack } from "@chakra-ui/react"
-import viteLogo from "/vite.svg"
+import { Center } from "@chakra-ui/react"
 
 function App() {
   return (
     <Center flexDir="column" gap="8" minH="dvh">
-      <HStack>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </HStack>
-      <HStack>
-        <Button>Click me</Button>
-        <Button>Click me</Button>
-      </HStack>
+      <Routes>
+        <Route path="/" element={<Shop />} />
+      </Routes>
     </Center>
   )
 }
