8e1f5681bd01f6ab0e6e0cf50f55082c0c1396af
Peter Hintze <hintze.peter@gmail.com>
2025-02-03 22:17:55 -0500

    Sign up page now accepts inputs

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -12,28 +12,44 @@ import {
 import { Field } from '../components/ui/field';
 
 const Signup = () => {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    };
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        // REPLACE WITH LOGIN DETAILS HERE
+        // REPLACE WITH SIGNUP DETAILS HERE
+        console.log('Name:', name);
         console.log('Email:', email);
         console.log('Password:', password);
     };
 
     return (
         <Center>
-            <Card.Root w="md">
+            <Card.Root as="form" w="md">
                 <Card.Header>
                     <Heading size="xl" textAlign="center">Sign Up</Heading>
                 </Card.Header>
                 <Card.Body p={6}>
-                    <Stack as="form" onSubmit={handleSubmit}>
+                    <Stack onSubmit={handleSubmit}>
                         <Field label="Name">
                             <Input
                                 name="name"
                                 type="text"
+                                value={name}
+                                onChange={handleNameChange}
                                 required
                             />
                         </Field>
@@ -41,6 +57,8 @@ const Signup = () => {
                             <Input
                                 name="email"
                                 type="email"
+                                value={email}
+                                onChange={handleEmailChange}
                                 required
                             />
                         </Field>
@@ -48,13 +66,15 @@ const Signup = () => {
                             <Input
                                 name="password"
                                 type="password"
+                                value={password}
+                                onChange={handlePasswordChange}
                                 required
                                 />
                         </Field>
                     </Stack>
                 </Card.Body>
-                <Card.Footer>
-                    <Button type="submit" colorPalette="teal" rounded="md" onClick={handleSubmit}>
+                <Card.Footer justifyContent="center">
+                    <Button type="submit" colorPalette="teal" rounded="md">
                         Create User Account
                     </Button>
                 </Card.Footer>
