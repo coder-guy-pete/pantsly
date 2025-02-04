86066817ff1d6cb48ec4b7c388e9be807227e856
Corey P <cparsons0730@yahoo.com>
2025-02-03 20:48:31 -0600

    Updated OrderItem model

--- server/src/models/OrderItem.js
+++ server/src/models/OrderItem.js
@@ -5,17 +5,18 @@ class OrderItem extends Model {}
 
 OrderItem.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     quantity: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 1,
     },
-    price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-    },
     //Foreign Key
-    orderId: {
+    order_Id: {
       type: DataTypes.INTEGER,
       references: {
         model: "orders",
