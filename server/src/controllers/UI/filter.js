2ccb48a25bbb38c4e55833e8aed3ebf94623053e
jankrajniak <jan.krajniak@gmail.com>
2025-02-04 21:01:37 -0500

    helper function folder created. Extracted product array generation from search, filter and product GET calls. Eliminated destructuring when importing models in the Seeding code

--- server/src/controllers/UI/filter.js
+++ server/src/controllers/UI/filter.js
@@ -1,11 +1,13 @@
 // import { Product } from '../../models/index.js';
+import { Op } from 'sequelize';
 import tempData from '../tempdata.js';
+import productResponse from '../utils/productResponse.js';
 
 // filter GET
-export const filterGet = (req, res) => {
+export const filterGet = async (req, res) => {
     res.json(tempData);
 
-    //THE CODE BELOW WILL REPLACE THE CODE ABOVE ONCE THE DATABASE IS PROPERLY CONNECTED
+    // //THE CODE BELOW WILL REPLACE THE CODE ABOVE ONCE THE DATABASE IS PROPERLY CONNECTED
     // try {
     //     const products = await Product.findAll({
     //     attributes: ['productId', 'product_name', 'brand', 'color', 'size', 'sell_price', 'stock_quantity', 'image_url'],
@@ -23,46 +25,13 @@ export const filterGet = (req, res) => {
     //     if (!products) {
     //         return res.json([]);
     //     } else {
-    //         const productMap = {};
+    //         const filteredProducts = productResponse(products);
 
-    //         products.forEach((product) => {
-    //         const {productId, product_name, brand, color, size, sell_price, stock_quantity, image_url} = product;
-
-    //         if (!productMap[productId]) {
-    //             productMap[productId] = {
-    //                 product_group_id: productId,
-    //                 name: product_name,
-    //                 image_url: image_url,
-    //                 brand: brand,
-    //                 price: sell_price,
-    //                 sizes: [],
-    //                 colors: {
-    //                     s: [],
-    //                     m: [],
-    //                     l: [],
-    //                     xl: [],
-    //                     xxl: [],
-    //                     xxxl: [],
-    //                 },
-    //                 description: '',
-    //                 quantity: stock_quantity,
-    //             }
-    //         }
-
-    //         if (!productMap[productId].sizes.includes(size)) {
-    //             productMap[productId].sizes.push(size);
-    //         }
-
-    //         if (!productMap[productId].colors[size].includes(color)) {
-    //             productMap[productId].colors[size].push(color);
-    //         }
-    //     });
-
-    //     return res.json(Object.values(productMap));
-    // }
+    //         return res.json(Object.values(filteredProducts));
+    //     }
     // } catch (error) {
     //     console.error(error);
-    //     res.status(500).json({message: 'Server error'});
+    //     res.status(500).json([]);
     // }
 
 }
