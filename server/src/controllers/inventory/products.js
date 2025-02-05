0fd673dba7efda8818b50318b8c9777a9b4882ba
jankrajniak <jan.krajniak@gmail.com>
2025-02-04 19:58:50 -0500

    filter and products API calls updated to return empty array when sequelize doesn't return any results

--- server/src/controllers/inventory/products.js
+++ server/src/controllers/inventory/products.js
@@ -5,54 +5,58 @@ import tempData from '../tempdata.js';
 export const productsGet = async (req, res) => {
     res.json(tempData);
 
-// THE CODE BELOW WILL REPLACE THE CODE ABOVE ONCE THE DATABASE IS PROPERLY CONNECTED
-//     try {
-//         const products = await Product.findAll({
-//         attributes: ['productId', 'product_name', 'brand', 'color', 'size', 'sell_price', 'stock_quantity', 'image_url'],
-//         raw: true,
-//     });
+//THE CODE BELOW WILL REPLACE THE CODE ABOVE ONCE THE DATABASE IS PROPERLY CONNECTED
+    // try {
+    //     const products = await Product.findAll({
+    //     attributes: ['productId', 'product_name', 'brand', 'color', 'size', 'sell_price', 'stock_quantity', 'image_url'],
+    //     raw: true,
+    // });
 
-//     const productMap = {};
+    // if (!products) {
+    //     return res.json([]);
+    // } else {
+    //     const productMap = {};
 
-//     products.forEach((product) => {
-//         const {productId, product_name, brand, color, size, sell_price, stock_quantity, image_url} = product;
+    //     products.forEach((product) => {
+    //         const {productId, product_name, brand, color, size, sell_price, stock_quantity, image_url} = product;
 
-//         if (!productMap[productId]) {
-//             productMap[productId] = {
-//                 product_group_id: productId,
-//                 name: product_name,
-//                 image_url: image_url,
-//                 brand: brand,
-//                 price: sell_price,
-//                 sizes: [],
-//                 colors: {
-//                     s: [],
-//                     m: [],
-//                     l: [],
-//                     xl: [],
-//                     xxl: [],
-//                     xxxl: [],
-//                 },
-//                 description: '',
-//                 quantity: stock_quantity,
-//             }
-//         }
+    //         if (!productMap[productId]) {
+    //             productMap[productId] = {
+    //                 product_group_id: productId,
+    //                 name: product_name,
+    //                 image_url: image_url,
+    //                 brand: brand,
+    //                 price: sell_price,
+    //                 sizes: [],
+    //                 colors: {
+    //                     s: [],
+    //                     m: [],
+    //                     l: [],
+    //                     xl: [],
+    //                     xxl: [],
+    //                     xxxl: [],
+    //                 },
+    //                 description: '',
+    //                 quantity: stock_quantity,
+    //             }
+    //         }
 
-//         if (!productMap[productId].sizes.includes(size)) {
-//             productMap[productId].sizes.push(size);
-//         }
+    //         if (!productMap[productId].sizes.includes(size)) {
+    //             productMap[productId].sizes.push(size);
+    //         }
 
-//         if (!productMap[productId].colors[size].includes(color)) {
-//             productMap[productId].colors[size].push(color);
-//         }
-//     });
+    //         if (!productMap[productId].colors[size].includes(color)) {
+    //             productMap[productId].colors[size].push(color);
+    //         }
+    //     });
 
-//     return res.json(Object.values(productMap));
+    //     return res.json(Object.values(productMap));
 
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({message: 'Server error'});
-//     }
+    // }
+    // } catch (error) {
+    //     console.error(error);
+    //     res.status(500).json({message: 'Server error'});
+    // }
 }
 
 
