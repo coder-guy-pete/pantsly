6b0bda5fe740b216f394103a0d5c2a660827a36f
Peter Hintze <hintze.peter@gmail.com>
2025-02-09 12:14:07 -0500

    auth routes

---
+++ server/src/routes/auth-routes.js
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import { User } from '../models/User.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+        return res.status(404).json({ message: 'Authentication failed' });
+    }
+
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+    if (!passwordIsValid) {
+        return res.status(401).json({ message: 'Authentication failed' });
+    }
+
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
+    const token = jwt.sign({ id: user.id, email: user.email }, secretKey, { expiresIn: '1h' });
+    return res.json({ token });
+};
+
+const router = Router();
+
+router.post('/login', login);
+
+export default router;
