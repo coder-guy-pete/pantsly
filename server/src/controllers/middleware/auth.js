a3c7c87fa9e7fcc4027b564f2677564aba6c8097
Peter Hintze <hintze.peter@gmail.com>
2025-02-09 12:05:43 -0500

    server side auth middleware

---
+++ server/src/controllers/middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const authToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const isAdmin = process.env.SECRET_ADMIN || '';
+
+    if (authHeader && authHeader === isAdmin) {
+        req.user = { email: 'admin@admin.com' };
+        return next();
+    }
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        const secretKey = process.env.JWT_SECRET_KEY || '';
+
+        jwt.verify(token, secretKey, (err, user) => {
+            if (err) {
+                return res.sendStatus(403).json({ message: 'Token expired or invavlid' });
+            }
+
+            req.user = user;
+            return next();
+        });
+    } else {
+        return res.sendStatus(401).json({ message: 'Unauthorized' });
+    }
